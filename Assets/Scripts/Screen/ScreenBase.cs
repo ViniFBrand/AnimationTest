284a184cfe302ef48281ca8af4235b6e2759c8dd
Vini <vfbrandx6@gmail.com>
2025-08-12 17:25:03 -0300

    Added Typing Animation to Text on Screen
    Adicionou animação de digitação ao texto na tela

--- Assets/Scripts/Screen/ScreenBase.cs
+++ Assets/Scripts/Screen/ScreenBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
 using DG.Tweening;
+using UnityEngine.UI;
 
 namespace Screens
 {
@@ -18,6 +19,7 @@ namespace Screens
         public ScreenType ScreenType;
 
         public List<Transform> listOfObjects;
+        public List<Typper> listOfSentences;
 
         public bool startHided = false;
 
@@ -54,14 +56,33 @@ namespace Screens
 
         private void ShowObjects()
         {
-            for(int i = 0; i< listOfObjects.Count; i++)
+
+            for (int i = 0; i< listOfObjects.Count; i++)
             {
                 var obj = listOfObjects[i];
 
                 obj.gameObject.SetActive(true);
                 obj.DOScale(0, animationDuration).From().SetDelay(i * delayBetweenObjects);
             }
+
+            Invoke(nameof(StartType), delayBetweenObjects * listOfObjects.Count);
         }
+
+        private void StartType()
+        {
+            for (int i = 0; i < listOfSentences.Count; i++)
+            {
+                listOfSentences[i].StartType();
+            }
+        }
+
+        /*private void StartUntype()
+        {
+            for (int i = 0; i < listOfSentences.Count; i++)
+            {
+                listOfSentences[i].StartUntype();
+            }
+        }*/
 
         private void ForceShowObjects()
         {
---
+++ Assets/Scripts/Utils/Typper.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Typper : MonoBehaviour
+{
+    public TextMeshProUGUI textMesh;
+    public float timeBetweenLetters = .1f;
+
+    public string sentence;
+
+    private void Awake()
+    {
+        textMesh.text = "";
+    }
+
+    [NaughtyAttributes.Button]
+    public void StartType()
+    {
+        StartCoroutine(Type(sentence));
+    }
+
+    /*public void StartUntype()
+    {
+        StartCoroutine(Untype(sentence));
+    }*/
+
+    IEnumerator Type(string s)
+    {
+        textMesh.text = "";
+        foreach(char l in s.ToCharArray())
+        {
+            textMesh.text += l;
+            yield return new WaitForSeconds(timeBetweenLetters);
+        }
+    }
+
+    /*IEnumerator Untype(string s)
+    {
+        string currentText = s;
+        while (currentText.Length > 0)
+        {
+            currentText = currentText.Substring(0, currentText.Length - 1);
+            textMesh.text = currentText;
+            yield return new WaitForSeconds(timeBetweenLetters);
+        }    
+    }*/
+}
