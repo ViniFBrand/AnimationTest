f3ac7d63e4fdfe3541f805c1e4a1e4a7943423fe
vincenteamaga <vfbrandx6@gmail.com>
2025-08-19 17:48:09 -0300

    Added Particles in Play Button and setup Settings Screen
    Adição de botão de Play e de partículas ao clicar no botão de Play
    Configuração do Settings Screen
    
    - Tentanto corrigir bug de texto continuar a digitar ao clicar em algum botão (se Typping não tiver acabado)

--- Assets/Scripts/Screen/ScreenHelper.cs
+++ Assets/Scripts/Screen/ScreenHelper.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,11 +10,16 @@ namespace Screens
     public class ScreenHelper : MonoBehaviour
     {
         public ScreenType screenType;
+        public Typper typper;
 
 
         public void OnClick()
         {
             ScreenManager.Instance.ShowByType(screenType);
+            /* TRYING TO STOP TYPPING AS CLICK BUTTON
+            var sentence = typper.textMesh.text = "";
+            typper.StartCoroutine(sentence);
+            */
         }
     }
 
